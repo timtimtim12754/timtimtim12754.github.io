c3737e18b38d721679f31b026243ff42a5bc8e37
pront <prontgo@gmail.com>
2024-10-06 16:34:13 +0800

    更新

---
+++ apps/lawapp/main.js
@@ -0,0 +1,3 @@
+document.getElementById("download").addEventListener("click",function(){
+    window.location.href = 'https://github.com/timtimtim12754/TaiwanLawApp/raw/refs/heads/main/lawApp-1.0.apk';
+})
