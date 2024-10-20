950a0c6cf128ec01c8e6495d791fa70c92825bc9
Roya <131457869+timtimtim12754@users.noreply.github.com>
2024-10-20 19:25:09 +0800

    更新

--- apps/lawapp/main.js
+++ apps/lawapp/main.js
@@ -1,3 +1,3 @@
 document.getElementById("download").addEventListener("click", function () {
-    window.location.href ="https://download1582.mediafire.com/ckc6yntzh4igx0HCOdWGR57H9_GrmkiLY59BsvFlo2CGrvVkyvHrUlDLdhozvZE8j2emPOc7q8ZVIgrxKTex4lGdMrxLFFCfLq4W9CaAklvWJ1AH8Seo5jTAcy1gxdxpq4fnrbrsm9J6i3b6nY08unh-fJE0TfZxCeoW3XcZw2kZ/i2ckc9skvzwx1mq/lawApp-1.1.apk";
+    window.location.href ="https://www.dropbox.com/scl/fi/ihh58jnehamclt61nly7d/lawApp-1.1.apk?rlkey=54hxqzxyr2a4xawli98cfrpuj&st=1fo4zn1m&dl=1";
 });
